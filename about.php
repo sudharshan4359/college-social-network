35e70ac65c6166eec9f68c1a01d1863369cb83b4
sudharshan4359 <132658295+sudharshan4359@users.noreply.github.com>
2024-05-29 19:41:37 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,29 @@
+<?php
+include("header.php")
+?>
+
+  <section id="contentSection">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="left_content">
+          <div class="contact_area">
+            <h2>About Us</h2>
+            <h1>Dr. M.G.R. Engineering College</h1>
+            <p>Dr. M.G.R. Engineering College  was established in 1998, by the Founder Chairman Thiru A C Shanmugam B.A., B.L., Ex M.P, Ex. M.L.A, a committed and dedicated Medical Professional.</p><p>
+            Dr. M.G.R. Educational and Research Institute has a total of 943 faculty members and its campus is spread over an area of 300 acres. Currently, 15852 students are enrolled in the various programmes offered by the institute in 15 UG courses, 8 PG Courses including MBA, MS by Research and Doctoral programs (PhD)  in five Departments.</p><p>
+National Board of Accredition NBA has Accredited 5 UG courses.
+Ranked 96 by NIRF- National Institute Ranking Framework for the academic year 2017-18 among all IITs, Central, State and Private Institutions in India. </p><p>
+Awarded 'A+' GRADE with a high score of 3.19 on a scale of 4 by the National Assessment and Accreditation Council (NAAC) for 5 Years.
+Dr. M.G.R. Engineering College awarded AUTONOMOUS status by the UGC from the academic year 2019-2020. </p><p>
+Dr. M.G.R. Engineering College is recognized as a Scientific and Industrial Research Organization (SIRO) by the Department of Scientific and Industrial Research (DSIR), Government of India.</p><p>
+Five Research Centres recognised by Anna University
+MoU's with Reputed Academic Institutions in India and Abroad. </p>
+             
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+ <?php
+ include("footer.php")
+ ?>
